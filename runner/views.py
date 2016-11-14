668551a20327321d00751214c1c86a8e1254041c
Daniel Schliffka <confluxhero@gmail.com>
2016-11-13 20:35:17 -0600

    runner view and template

--- runner/views.py
+++ runner/views.py
@@ -1,7 +1,23 @@
+'''
 from django.shortcuts import render
-from .models import Runner
 
 # Create your views here.
 def index(request):
     roster = Runner.objects.all()
     return render( request, 'runner/index.html', {'roster': roster})
+'''
+
+
+from django.views import generic
+from .models import Runner
+
+class IndexView(generic.ListView):
+    template_name = 'runner/index.html'
+    context_object_name = 'roster'
+
+    def get_queryset(self):
+        return Runner.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Runner
+    #template_name = 'runner/detail.html'
